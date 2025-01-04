2a6a2d391ee4570e8be85f35b354e11740c69b57
scssw <90193529+scssw@users.noreply.github.com>
2025-01-04 08:21:04 +0800

    Update rednsmasq.sh

--- rednsmasq.sh
+++ rednsmasq.sh
@@ -7,35 +7,35 @@ address=/fast.com/127.0.0.1
 address=/.fast.com/127.0.0.1
 
 # Netflix 专用 DNS
-server=/netflix.ca/45.39.199.32
-server=/netflix.com/45.39.199.32
-server=/netflix.net/45.39.199.32
-server=/netflixinvestor.com/45.39.199.32
-server=/netflixtechblog.com/45.39.199.32
-server=/nflxext.com/45.39.199.32
-server=/nflximg.com/45.39.199.32
-server=/nflximg.net/45.39.199.32
-server=/nflxsearch.net/45.39.199.32
-server=/nflxso.net/45.39.199.32
-server=/nflxvideo.net/45.39.199.32
-server=/netflixdnstest0.com/45.39.199.32
-server=/netflixdnstest1.com/45.39.199.32
-server=/netflixdnstest2.com/45.39.199.32
-server=/netflixdnstest3.com/45.39.199.32
-server=/netflixdnstest4.com/45.39.199.32
-server=/netflixdnstest5.com/45.39.199.32
-server=/netflixdnstest6.com/45.39.199.32
-server=/netflixdnstest7.com/45.39.199.32
-server=/netflixdnstest8.com/45.39.199.32
-server=/netflixdnstest9.com/45.39.199.32
-server=/cinemax.com/45.39.199.32
-server=/forthethrone.com/45.39.199.32
+server=/netflix.ca/156.229.167.246
+server=/netflix.com/156.229.167.246
+server=/netflix.net/156.229.167.246
+server=/netflixinvestor.com/156.229.167.246
+server=/netflixtechblog.com/156.229.167.246
+server=/nflxext.com/156.229.167.246
+server=/nflximg.com/156.229.167.246
+server=/nflximg.net/156.229.167.246
+server=/nflxsearch.net/156.229.167.246
+server=/nflxso.net/156.229.167.246
+server=/nflxvideo.net/156.229.167.246
+server=/netflixdnstest0.com/156.229.167.246
+server=/netflixdnstest1.com/156.229.167.246
+server=/netflixdnstest2.com/156.229.167.246
+server=/netflixdnstest3.com/156.229.167.246
+server=/netflixdnstest4.com/156.229.167.246
+server=/netflixdnstest5.com/156.229.167.246
+server=/netflixdnstest6.com/156.229.167.246
+server=/netflixdnstest7.com/156.229.167.246
+server=/netflixdnstest8.com/156.229.167.246
+server=/netflixdnstest9.com/156.229.167.246
+server=/cinemax.com/156.229.167.246
+server=/forthethrone.com/156.229.167.246
 
 # 迪士尼+ 域名通过解锁 DNS 解析
-server=/disneyplus.com/45.39.199.32
-server=/dssott.com/45.39.199.32
-server=/bamgrid.com/45.39.199.32
-server=/akamaized.net/45.39.199.32
+server=/disneyplus.com/156.229.167.246
+server=/dssott.com/156.229.167.246
+server=/bamgrid.com/156.229.167.246
+server=/akamaized.net/156.229.167.246
 
 # 其他域名通过 Google DNS 解析
 server=8.8.8.8
