31d478a22ad558404c8e7ac4815706a002269518
scssw <90193529+scssw@users.noreply.github.com>
2025-01-04 16:26:34 +0800

    Create setup_root.sh

---
+++ setup_root.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo "设置你的ROOT密码"
+passwd
+sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+service sshd restart
+echo "ROOT登录设置完毕！"
