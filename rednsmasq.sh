d4a9e865336eb81e12037a8f289b03a15a4c9afc
scssw <90193529+scssw@users.noreply.github.com>
2025-01-23 19:22:11 +0800

    Update rednsmasq.sh

--- rednsmasq.sh
+++ rednsmasq.sh
@@ -6,56 +6,45 @@ cat <<EOF | sudo tee /etc/dnsmasq.conf > /dev/null
 address=/fast.com/127.0.0.1
 address=/.fast.com/127.0.0.1
 
+# 禁止测速
+address=/fast.com/127.0.0.1
+address=/.fast.com/127.0.0.1
+
 # Netflix 专用 DNS
-server=/netflix.ca/156.229.167.246
-server=/netflix.com/156.229.167.246
-server=/netflix.net/156.229.167.246
-server=/netflixinvestor.com/156.229.167.246
-server=/netflixtechblog.com/156.229.167.246
-server=/nflxext.com/156.229.167.246
-server=/nflximg.com/156.229.167.246
-server=/nflximg.net/156.229.167.246
-server=/nflxsearch.net/156.229.167.246
-server=/nflxso.net/156.229.167.246
-server=/nflxvideo.net/156.229.167.246
-server=/netflixdnstest0.com/156.229.167.246
-server=/netflixdnstest1.com/156.229.167.246
-server=/netflixdnstest2.com/156.229.167.246
-server=/netflixdnstest3.com/156.229.167.246
-server=/netflixdnstest4.com/156.229.167.246
-server=/netflixdnstest5.com/156.229.167.246
-server=/netflixdnstest6.com/156.229.167.246
-server=/netflixdnstest7.com/156.229.167.246
-server=/netflixdnstest8.com/156.229.167.246
-server=/netflixdnstest9.com/156.229.167.246
-server=/cinemax.com/156.229.167.246
-server=/forthethrone.com/156.229.167.246
-# ChatGPT DNS解锁
-server=/openai.com/156.229.167.246
-server=/chatgpt.com/156.229.167.246
-server=/chat.com/156.229.167.246
-server=/oaistatic.com/156.229.167.246
-server=/oaiusercontent.com/156.229.167.246
-server=/chat.comopenai.com.cdn.cloudflare.net/156.229.167.246
-server=/openaiapi-site.azureedge.net/156.229.167.246
-server=/openaicom-api-bdcpf8c6d2e9atf6.z01.azurefd.net/156.229.167.246
-server=/openaicomproductionae4b.blob.core.windows.net/156.229.167.246
-server=/production-openaicom-storage.azureedge.net/156.229.167.246
-server=/byteoversea.com/156.229.167.246
-server=/ibytedtos.com/156.229.167.246
-server=/ipstatp.com/156.229.167.246
-server=/muscdn.com/156.229.167.246
-server=/musical.ly/156.229.167.246
+server=/netflix.ca/5.231.70.121
+server=/netflix.com/5.231.70.121
+server=/netflix.net/5.231.70.121
+server=/netflixinvestor.com/5.231.70.121
+server=/netflixtechblog.com/5.231.70.121
+server=/nflxext.com/5.231.70.121
+server=/nflximg.com/5.231.70.121
+server=/nflximg.net/5.231.70.121
+server=/nflxsearch.net/5.231.70.121
+server=/nflxso.net/5.231.70.121
+server=/nflxvideo.net/5.231.70.121
+server=/netflixdnstest0.com/5.231.70.121
+server=/netflixdnstest1.com/5.231.70.121
+server=/netflixdnstest2.com/5.231.70.121
+server=/netflixdnstest3.com/5.231.70.121
+server=/netflixdnstest4.com/5.231.70.121
+server=/netflixdnstest5.com/5.231.70.121
+server=/netflixdnstest6.com/5.231.70.121
+server=/netflixdnstest7.com/5.231.70.121
+server=/netflixdnstest8.com/5.231.70.121
+server=/netflixdnstest9.com/5.231.70.121
+server=/cinemax.com/5.231.70.121
+server=/forthethrone.com/5.231.70.121
 
 # 迪士尼+ 域名通过解锁 DNS 解析
-server=/disneyplus.com/156.229.167.246
-server=/dssott.com/156.229.167.246
-server=/bamgrid.com/156.229.167.246
-server=/akamaized.net/156.229.167.246
+server=/disneyplus.com/5.231.70.121
+server=/dssott.com/5.231.70.121
+server=/bamgrid.com/5.231.70.121
+server=/akamaized.net/5.231.70.121
 
 # 其他域名通过 Google DNS 解析
 server=8.8.8.8
 server=1.1.1.1
+
 
 # 配置目录
 conf-dir=/etc/dnsmasq.d
