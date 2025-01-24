f5dd6549d95289ca9591adcda536df132bda1807
scssw <90193529+scssw@users.noreply.github.com>
2025-01-24 17:31:59 +0800

    Update rednsmasq.sh

--- rednsmasq.sh
+++ rednsmasq.sh
@@ -7,42 +7,42 @@ address=/fast.com/127.0.0.1
 address=/.fast.com/127.0.0.1
 
 # Netflix 专用 DNS
-server=/netflix.ca/5.231.70.121
-server=/netflix.com/5.231.70.121
-server=/netflix.net/5.231.70.121
-server=/netflixinvestor.com/5.231.70.121
-server=/netflixtechblog.com/5.231.70.121
-server=/nflxext.com/5.231.70.121
-server=/nflximg.com/5.231.70.121
-server=/nflximg.net/5.231.70.121
-server=/nflxsearch.net/5.231.70.121
-server=/nflxso.net/5.231.70.121
-server=/nflxvideo.net/5.231.70.121
-server=/netflixdnstest0.com/5.231.70.121
-server=/netflixdnstest1.com/5.231.70.121
-server=/netflixdnstest2.com/5.231.70.121
-server=/netflixdnstest3.com/5.231.70.121
-server=/netflixdnstest4.com/5.231.70.121
-server=/netflixdnstest5.com/5.231.70.121
-server=/netflixdnstest6.com/5.231.70.121
-server=/netflixdnstest7.com/5.231.70.121
-server=/netflixdnstest8.com/5.231.70.121
-server=/netflixdnstest9.com/5.231.70.121
-server=/cinemax.com/5.231.70.121
-server=/forthethrone.com/5.231.70.121
+server=/netflix.ca/109.176.203.10
+server=/netflix.com/109.176.203.10
+server=/netflix.net/109.176.203.10
+server=/netflixinvestor.com/109.176.203.10
+server=/netflixtechblog.com/109.176.203.10
+server=/nflxext.com/109.176.203.10
+server=/nflximg.com/109.176.203.10
+server=/nflximg.net/109.176.203.10
+server=/nflxsearch.net/109.176.203.10
+server=/nflxso.net/109.176.203.10
+server=/nflxvideo.net/109.176.203.10
+server=/netflixdnstest0.com/109.176.203.10
+server=/netflixdnstest1.com/109.176.203.10
+server=/netflixdnstest2.com/109.176.203.10
+server=/netflixdnstest3.com/109.176.203.10
+server=/netflixdnstest4.com/109.176.203.10
+server=/netflixdnstest5.com/109.176.203.10
+server=/netflixdnstest6.com/109.176.203.10
+server=/netflixdnstest7.com/109.176.203.10
+server=/netflixdnstest8.com/109.176.203.10
+server=/netflixdnstest9.com/109.176.203.10
+server=/cinemax.com/109.176.203.10
+server=/forthethrone.com/109.176.203.10
 
 # 迪士尼+ 域名通过解锁 DNS 解析
-server=/disneyplus.com/5.231.70.121
-server=/dssott.com/5.231.70.121
-server=/bamgrid.com/5.231.70.121
-server=/akamaized.net/5.231.70.121
+server=/disneyplus.com/109.176.203.10
+server=/dssott.com/109.176.203.10
+server=/bamgrid.com/109.176.203.10
+server=/akamaized.net/109.176.203.10
 
 # Gemini DNS解锁
-server=/gemini.google.com/5.231.70.121
-server=/proactivebackend-pa.googleapis.com/5.231.70.121
-server=/alkalimakersuite-pa.clients6.google.com/5.231.70.121
-server=/aistudio.google.com/5.231.70.121
-server=/generativelanguage.googleapis.com/5.231.70.121
+server=/gemini.google.com/142.171.209.200
+server=/proactivebackend-pa.googleapis.com/142.171.209.200
+server=/alkalimakersuite-pa.clients6.google.com/142.171.209.200
+server=/aistudio.google.com/142.171.209.200
+server=/generativelanguage.googleapis.com/142.171.209.200
 
 # 其他域名通过 Google DNS 解析
 server=8.8.8.8
