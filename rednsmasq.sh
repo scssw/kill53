7981e9d4ea8241a9d48a436d1ac5ff50e4e02052
scssw <90193529+scssw@users.noreply.github.com>
2025-01-26 14:11:05 +0800

    Update rednsmasq.sh

--- rednsmasq.sh
+++ rednsmasq.sh
@@ -7,35 +7,35 @@ address=/fast.com/127.0.0.1
 address=/.fast.com/127.0.0.1
 
 # Netflix 专用 DNS
-server=/netflix.ca/5.231.70.121
-server=/netflix.com/5.231.70.121
-server=/netflix.net/5.231.70.121
-server=/netflixinvestor.com/5.231.70.121
-server=/netflixtechblog.com/5.231.70.121
-server=/nflxext.com/5.231.70.121
-server=/nflximg.com/5.231.70.121
-server=/nflximg.net/5.231.70.121
-server=/nflxsearch.net/5.231.70.121
-server=/nflxso.net/5.231.70.121
-server=/nflxvideo.net/5.231.70.121
-server=/netflixdnstest0.com/5.231.70.121
-server=/netflixdnstest1.com/5.231.70.121
-server=/netflixdnstest2.com/5.231.70.121
-server=/netflixdnstest3.com/5.231.70.121
-server=/netflixdnstest4.com/5.231.70.121
-server=/netflixdnstest5.com/5.231.70.121
-server=/netflixdnstest6.com/5.231.70.121
-server=/netflixdnstest7.com/5.231.70.121
-server=/netflixdnstest8.com/5.231.70.121
-server=/netflixdnstest9.com/5.231.70.121
-server=/cinemax.com/5.231.70.121
-server=/forthethrone.com/5.231.70.121
+server=/netflix.ca/5.231.70.151
+server=/netflix.com/5.231.70.151
+server=/netflix.net/5.231.70.151
+server=/netflixinvestor.com/5.231.70.151
+server=/netflixtechblog.com/5.231.70.151
+server=/nflxext.com/5.231.70.151
+server=/nflximg.com/5.231.70.151
+server=/nflximg.net/5.231.70.151
+server=/nflxsearch.net/5.231.70.151
+server=/nflxso.net/5.231.70.151
+server=/nflxvideo.net/5.231.70.151
+server=/netflixdnstest0.com/5.231.70.151
+server=/netflixdnstest1.com/5.231.70.151
+server=/netflixdnstest2.com/5.231.70.151
+server=/netflixdnstest3.com/5.231.70.151
+server=/netflixdnstest4.com/5.231.70.151
+server=/netflixdnstest5.com/5.231.70.151
+server=/netflixdnstest6.com/5.231.70.151
+server=/netflixdnstest7.com/5.231.70.151
+server=/netflixdnstest8.com/5.231.70.151
+server=/netflixdnstest9.com/5.231.70.151
+server=/cinemax.com/5.231.70.151
+server=/forthethrone.com/5.231.70.151
 
 # 迪士尼+ 域名通过解锁 DNS 解析
-server=/disneyplus.com/5.231.70.121
-server=/dssott.com/5.231.70.121
-server=/bamgrid.com/5.231.70.121
-server=/akamaized.net/5.231.70.121
+server=/disneyplus.com/5.231.70.151
+server=/dssott.com/5.231.70.151
+server=/bamgrid.com/5.231.70.151
+server=/akamaized.net/5.231.70.151
 
 # Gemini DNS解锁
 server=/gemini.google.com/142.171.209.200
